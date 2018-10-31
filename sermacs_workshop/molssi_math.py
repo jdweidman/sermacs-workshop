b85d201fb1c83d4bf775e31fc7660750450f9674
jdweidman <jweidman@uga.edu>
2018-10-31 14:05:50 -0400

    added error checking for mean function

--- sermacs_workshop/molssi_math.py
+++ sermacs_workshop/molssi_math.py
@@ -19,12 +19,33 @@ def mean(num_list):
     mean: float
         Mean of list of numbers
     """
+    
+    #Check that input is type list
+    if not isinstance(num_list, list):
+        raise TypeError('Input must be type list') 
+
+    #Check that list has length
+    if len(num_list) == 0:
+        raise IndexError('Cannot apply mean function to empty list')
+
+    #Check that list contains numbers
+    #for num in num_list:
+    #    if isinstance(num, int) or isinstance(num, float):
+    #        pass
+    #    else:
+    #        raise TypeError('Input list must consist only of numbers')
+
     #sum = 0.0
     #for num in num_list:
     #    sum += num
 
     #mean = sum/len(num_list)
     #return mean
+    
+    try:
+        mean = sum(num_list)/len(num_list)
+    except TypeError:
+        raise TypeError('Input list must consist only of ints or floats')
+        
 
-    mean = sum(num_list)/len(num_list)
     return mean
